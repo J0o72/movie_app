1cb09b68b1fd76d7cfccfcf1610d73c4bb57b616
Youssef <youssefabdallah2016@gmail.com>
2024-09-30 21:22:09 +0300

    create custom bookmark icon & and added it to all posters

---
+++ lib/Features/home_feature/presentation/widgets/custom_bookmark_icon.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/constants.dart';
+
+class CustomBookmarkIcon extends StatelessWidget {
+  const CustomBookmarkIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 5,
+      top: 5,
+      child: CircleAvatar(
+        backgroundColor: kMainColor.withOpacity(0.5),
+        child: IconButton(
+          padding: EdgeInsets.zero,
+          alignment: Alignment.center,
+          onPressed: () {},
+          icon: const Icon(
+            Icons.bookmark_border,
+            color: Colors.white,
+            size: 32,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home_feature/presentation/widgets/custom_poster.dart
+++ lib/Features/home_feature/presentation/widgets/custom_poster.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:movie_app/Features/home_feature/presentation/widgets/custom_bookmark_icon.dart';
 import 'package:movie_app/Features/home_feature/presentation/widgets/custom_circular_loading.dart';
+import 'package:movie_app/constants.dart';
 
 class CustomPoster extends StatelessWidget {
   const CustomPoster({
@@ -9,23 +12,28 @@ class CustomPoster extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: AspectRatio(
-          aspectRatio: 2.8 / 3.5,
-          child: CachedNetworkImage(
-            fit: BoxFit.cover,
-            imageUrl:
-                'https://image.tmdb.org/t/p/original/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg',
-            errorWidget: (context, url, error) => const Icon(Icons.error),
-            placeholder: (context, url) => const Center(
-              child: CustomCircularLoading(),
+    return Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.25,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: AspectRatio(
+              aspectRatio: 2.8 / 3.5,
+              child: CachedNetworkImage(
+                fit: BoxFit.cover,
+                imageUrl:
+                    'https://image.tmdb.org/t/p/original/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg',
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                placeholder: (context, url) => const Center(
+                  child: CustomCircularLoading(),
+                ),
+              ),
             ),
           ),
         ),
-      ),
+        const CustomBookmarkIcon(),
+      ],
     );
   }
 }
