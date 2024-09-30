601e54de07a80272441c142217ce38dce30e7aa4
Youssef <youssefabdallah2016@gmail.com>
2024-09-30 22:12:23 +0300

    add saved view to appRoutes

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -2,12 +2,14 @@ import 'package:go_router/go_router.dart';
 import 'package:movie_app/Features/home_feature/presentation/details_view.dart';
 import 'package:movie_app/Features/home_feature/presentation/genre_view.dart';
 import 'package:movie_app/Features/home_feature/presentation/home_view.dart';
+import 'package:movie_app/Features/home_feature/presentation/saved_view.dart';
 import 'package:movie_app/Features/welcome_feature/presentation/views/welcome_view.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kGenreView = '/genreView';
-  static const kDetailsView = '/derailsView';
+  static const kDetailsView = '/detailsView';
+  static const kSavedView = '/savedView';
 
   static final router = GoRouter(
     routes: [
@@ -26,6 +28,10 @@ abstract class AppRouter {
       GoRoute(
         path: kDetailsView,
         builder: (context, state) => const DetailsView(),
+      ),
+      GoRoute(
+        path: kSavedView,
+        builder: (context, state) => const SavedView(),
       ),
     ],
   );
