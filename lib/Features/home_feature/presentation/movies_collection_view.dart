81a61b8c2e5fbb86a8d45cc7985d012ce2e466f1
Youssef <youssefabdallah2016@gmail.com>
2024-10-02 11:45:56 +0300

    create movie collection body

---
+++ lib/Features/home_feature/presentation/movies_collection_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/Features/home_feature/presentation/widgets/movie_collection_view_body.dart';
+
+class MovieCollecionView extends StatelessWidget {
+  const MovieCollecionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MovieCollecionViewBody();
+  }
+}
---
+++ lib/Features/home_feature/presentation/widgets/movie_collection_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MovieCollecionViewBody extends StatelessWidget {
+  const MovieCollecionViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
