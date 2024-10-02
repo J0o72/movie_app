e00afe151649b57613ace3132f154148da020062
Youssef <youssefabdallah2016@gmail.com>
2024-10-02 19:18:30 +0300

    create actor profile view

---
+++ lib/Features/home_feature/presentation/actors_profile_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/Features/home_feature/presentation/widgets/actor_profile_view_body.dart';
+
+class ActorProfileView extends StatelessWidget {
+  const ActorProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ActorProfileViewBody();
+  }
+}
---
+++ lib/Features/home_feature/presentation/widgets/actor_profile_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ActorProfileViewBody extends StatelessWidget {
+  const ActorProfileViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
