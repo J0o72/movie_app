61d88be6699234af4e2c6fd92c090fc683368f18
Youssef <youssefabdallah2016@gmail.com>
2024-10-02 12:48:56 +0300

    go to details view from movieCollection and saved view

--- lib/Features/home_feature/presentation/widgets/custom_poster_saved.dart
+++ lib/Features/home_feature/presentation/widgets/custom_poster_saved.dart
@@ -1,25 +1,32 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:movie_app/Features/home_feature/presentation/widgets/custom_circular_loading.dart';
+import 'package:movie_app/core/utils/app_routes.dart';
 
 class CutsomPosterSaved extends StatelessWidget {
   const CutsomPosterSaved({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: AspectRatio(
-          aspectRatio: 2.8 / 3.5,
-          child: CachedNetworkImage(
-            fit: BoxFit.cover,
-            imageUrl:
-                'https://image.tmdb.org/t/p/original/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg',
-            errorWidget: (context, url, error) => const Icon(Icons.error),
-            placeholder: (context, url) => const Center(
-              child: CustomCircularLoading(),
+    return InkWell(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kDetailsView);
+      },
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.25,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: AspectRatio(
+            aspectRatio: 2.8 / 3.5,
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              imageUrl:
+                  'https://image.tmdb.org/t/p/original/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg',
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+              placeholder: (context, url) => const Center(
+                child: CustomCircularLoading(),
+              ),
             ),
           ),
         ),
