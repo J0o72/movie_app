6cfd7978b5904bd307157d7cd58e7cfa7104e696
Youssef <youssefabdallah2016@gmail.com>
2024-10-04 10:51:26 +0300

    Create Service Locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:movie_app/Features/home_feature/data/repos/movie_repo/movie_repo_impl.dart';
+import 'package:movie_app/Features/home_feature/data/repos/tv_shows_repo/tv_shows_repo_impl.dart';
+import 'package:movie_app/core/utils/api_service.dart';
+
+final getIt = GetIt.instance;
+
+void setupServerLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+
+  getIt
+      .registerSingleton<MovieRepoImpl>(MovieRepoImpl(getIt.get<ApiService>()));
+
+  getIt.registerSingleton<TVShowsRepoImpl>(TVShowsRepoImpl(ApiService(Dio())));
+}
