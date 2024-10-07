225127d36e2baaf32fc3f7faba27643245fa43d6
Youssef <youssefabdallah2016@gmail.com>
2024-10-07 11:03:55 +0300

    details state & cubit Done

---
+++ lib/Features/home_feature/presentation/manager/details_cubit/details_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/Features/home_feature/data/models/details_model/details_model/details_model.dart';
+import 'package:movie_app/Features/home_feature/data/repos/details_repo/details_repo.dart';
+
+part 'details_state.dart';
+
+class DetailsCubit extends Cubit<DetailsState> {
+  DetailsCubit(this.detailsRepo) : super(DetailsInitial());
+
+  final DetailsRepo detailsRepo;
+
+  Future<void> fetchDetails({required int id}) async {
+    emit(DetailsLoading());
+    var result = await detailsRepo.fetchDetails(id: id);
+
+    result.fold((failure) {
+      emit(
+        DetailsFailure(errorMessage: failure.errMessage),
+      );
+    }, (details) {
+      emit(
+        DetailsSuccess(detailsModel: details),
+      );
+    });
+  }
+}
