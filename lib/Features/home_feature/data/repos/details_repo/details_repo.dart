339f904cc552d65e9a9ab73d28d08d5a51873c48
Youssef <youssefabdallah2016@gmail.com>
2024-10-07 12:53:20 +0300

    update details repo to impl fetch casts

--- lib/Features/home_feature/data/repos/details_repo/details_repo.dart
+++ lib/Features/home_feature/data/repos/details_repo/details_repo.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:movie_app/Features/home_feature/data/models/details_model/casts_model.dart';
 import 'package:movie_app/Features/home_feature/data/models/details_model/details_model/details_model.dart';
 import 'package:movie_app/core/errors/failure.dart';
 
 abstract class DetailsRepo {
   Future<Either<Failure, DetailsModel>> fetchDetails({required int id});
+  Future<Either<Failure, List<CastsModel>>> fetchDetailsCast({required int id});
 }
--- lib/Features/home_feature/data/repos/details_repo/details_repo_impl.dart
+++ lib/Features/home_feature/data/repos/details_repo/details_repo_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:movie_app/Features/home_feature/data/models/details_model/casts_model.dart';
 import 'package:movie_app/Features/home_feature/data/models/details_model/details_model/details_model.dart';
 import 'package:movie_app/Features/home_feature/data/repos/details_repo/details_repo.dart';
 import 'package:movie_app/core/errors/failure.dart';
@@ -15,6 +16,29 @@ class DetailsRepoImpl implements DetailsRepo {
       var data = await apiService.get(endPoint: 'movie/$id?language=en-US');
 
       return right(DetailsModel.fromJson(data));
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<CastsModel>>> fetchDetailsCast(
+      {required int id}) async {
+    try {
+      var data =
+          await apiService.get(endPoint: 'movie/$id/credits?language=en-US');
+
+      List<CastsModel> casts = [];
+
+      for (var item in data['cast']) {
+        casts.add(CastsModel.fromJson(item));
+      }
+
+      return right(casts);
     } catch (e) {
       if (e is DioException) {
         return left(ServerFailure.fromDioError(e));
