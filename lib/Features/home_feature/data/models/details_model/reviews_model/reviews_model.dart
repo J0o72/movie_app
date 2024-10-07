7f20382c079791463d129132cf5e609860845e4e
Youssef <youssefabdallah2016@gmail.com>
2024-10-07 14:09:30 +0300

    add reviews model

---
+++ lib/Features/home_feature/data/models/details_model/reviews_model/reviews_model.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+import 'result.dart';
+
+class ReviewsModel extends Equatable {
+  final int? id;
+  final int? page;
+  final List<Result>? results;
+  final int? totalPages;
+  final int? totalResults;
+
+  const ReviewsModel({
+    this.id,
+    this.page,
+    this.results,
+    this.totalPages,
+    this.totalResults,
+  });
+
+  factory ReviewsModel.fromJson(Map<String, dynamic> json) => ReviewsModel(
+        id: json['id'] as int?,
+        page: json['page'] as int?,
+        results: (json['results'] as List<dynamic>?)
+            ?.map((e) => Result.fromJson(e as Map<String, dynamic>))
+            .toList(),
+        totalPages: json['total_pages'] as int?,
+        totalResults: json['total_results'] as int?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'page': page,
+        'results': results?.map((e) => e.toJson()).toList(),
+        'total_pages': totalPages,
+        'total_results': totalResults,
+      };
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      page,
+      results,
+      totalPages,
+      totalResults,
+    ];
+  }
+}
