530dde8decd95f19b64a4c6e0c2f111f37df51a0
Youssef <youssefabdallah2016@gmail.com>
2024-10-07 11:06:17 +0300

    refacor the code

--- lib/Features/home_feature/presentation/widgets/details_view_body.dart
+++ lib/Features/home_feature/presentation/widgets/details_view_body.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/Features/home_feature/presentation/widgets/custom_back_arrow_icon.dart';
-import 'package:movie_app/Features/home_feature/presentation/widgets/custom_bookmark_icon.dart';
-import 'package:movie_app/Features/home_feature/presentation/widgets/details_custom_poster.dart';
-import 'package:movie_app/Features/home_feature/presentation/widgets/details_view_body_information.dart';
+import 'package:movie_app/Features/home_feature/presentation/widgets/details_view_body_content.dart';
 
 class DetailsViewBody extends StatelessWidget {
   const DetailsViewBody({super.key});
@@ -11,48 +8,6 @@ class DetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: DetailsViewBodyContent(),
-    );
-  }
-}
-
-class DetailsViewBodyContent extends StatelessWidget {
-  const DetailsViewBodyContent({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              const Stack(
-                children: [
-                  DetailsCustomPoster(),
-                  CustomBookmarkIcon(
-                    rightPos: 20,
-                    topPos: 40,
-                  ),
-                  CustomBackArrowIcon(),
-                ],
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.5),
-                  boxShadow: const [
-                    BoxShadow(
-                      blurRadius: 150,
-                      color: Colors.black,
-                      offset: Offset(0, -100),
-                      spreadRadius: 5,
-                    ),
-                  ],
-                ),
-                child: const DetailsViewBodyInformation(),
-              ),
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
