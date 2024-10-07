a460ec19ccc867f7caeced2629bf9d46cc9f363b
Youssef <youssefabdallah2016@gmail.com>
2024-10-07 10:53:04 +0300

    details repo & its implementation Done

---
+++ lib/Features/home_feature/data/repos/details_repo/details_repo_impl.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:movie_app/Features/home_feature/data/models/details_model/details_model/details_model.dart';
+import 'package:movie_app/Features/home_feature/data/repos/details_repo/details_repo.dart';
+import 'package:movie_app/core/errors/failure.dart';
+import 'package:movie_app/core/utils/api_service.dart';
+
+class DetailsRepoImpl implements DetailsRepo {
+  final ApiService apiService;
+
+  DetailsRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, DetailsModel>> fetchDetails({required int id}) async {
+    try {
+      var data = await apiService.get(endPoint: 'movie/$id?language=en-US');
+
+      return right(DetailsModel.fromJson(data));
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+}
