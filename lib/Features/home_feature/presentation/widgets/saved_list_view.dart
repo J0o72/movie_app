bd3248b15402e1a2ad077b1a04ffbe1f096c4717
Youssef <youssefabdallah2016@gmail.com>
2024-10-15 02:44:13 +0300

    Provide saved cubit to screen & make blocBuilder

--- lib/Features/home_feature/presentation/widgets/saved_list_view.dart
+++ lib/Features/home_feature/presentation/widgets/saved_list_view.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/Features/home_feature/presentation/manager/saved_cubit/saved_cubit.dart';
+import 'package:movie_app/Features/home_feature/presentation/widgets/custom_circular_loading.dart';
 import 'package:movie_app/Features/home_feature/presentation/widgets/saved_view_item.dart';
+import 'package:movie_app/core/widgets/custom_error_failure.dart';
 
 class SavedListView extends StatelessWidget {
   const SavedListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return const SavedViewItem();
+    return BlocBuilder<SavedCubit, SavedState>(
+      builder: (context, state) {
+        if (state is SavedSuccess) {
+          return ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return const SavedViewItem();
+            },
+          );
+        } else if (state is SavedFailure) {
+          return const CustomErrorFailure();
+        } else {
+          return const CustomCircularLoading();
+        }
       },
     );
   }
--- lib/Features/home_feature/presentation/widgets/saved_view_body.dart
+++ lib/Features/home_feature/presentation/widgets/saved_view_body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/Features/home_feature/data/repos/saved_repo/saved_repo_impl.dart';
+import 'package:movie_app/Features/home_feature/presentation/manager/saved_cubit/saved_cubit.dart';
 import 'package:movie_app/Features/home_feature/presentation/widgets/saved_list_view.dart';
+import 'package:movie_app/core/utils/service_locator.dart';
 
 import 'package:movie_app/core/utils/styles.dart';
 
@@ -8,17 +12,20 @@ class SavedViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const SavedListView(),
-      appBar: AppBar(
-        title: const Text(
-          "Saved",
-          style: Styles.styleText22,
+    return BlocProvider(
+      create: (context) => SavedCubit(getIt.get<SavedRepoImpl>()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Saved",
+            style: Styles.styleText22,
+          ),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          automaticallyImplyLeading: false,
         ),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        automaticallyImplyLeading: false,
+        body: const SavedListView(),
       ),
     );
   }
