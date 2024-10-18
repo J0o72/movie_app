31ae520da35d0e03df85ba529a28ebd91236f3de
Youssef <youssefabdallah2016@gmail.com>
2024-10-18 14:38:30 +0300

    DetailsReviewsSkeletonizer done

--- lib/Features/home_feature/presentation/widgets/details_review_list_view.dart
+++ lib/Features/home_feature/presentation/widgets/details_review_list_view.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/Features/home_feature/presentation/manager/reviews_cubit/reviews_cubit.dart';
 import 'package:movie_app/Features/home_feature/presentation/widgets/custom_circular_loading.dart';
 import 'package:movie_app/Features/home_feature/presentation/widgets/details_review_item.dart';
+import 'package:movie_app/constants.dart';
 import 'package:movie_app/core/utils/styles.dart';
 import 'package:movie_app/core/widgets/custom_error_failure.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class DetailsReviewListView extends StatelessWidget {
   const DetailsReviewListView({super.key});
@@ -37,8 +39,85 @@ class DetailsReviewListView extends StatelessWidget {
         } else if (state is ReviewsFailure) {
           return const CustomErrorFailure();
         } else {
-          return const CustomCircularLoading();
+          return const DetailsReviewsSkeletonizer();
         }
+      },
+    );
+  }
+}
+
+class DetailsReviewsSkeletonizer extends StatelessWidget {
+  const DetailsReviewsSkeletonizer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 2,
+      itemBuilder: (context, index) {
+        return Skeletonizer(
+          enabled: true,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  // color: Colors.white,
+                ),
+                width: 70,
+                height: 70,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.asset(
+                    'assets/images/movie_welcome_screen_bg.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'youssef abdallah',
+                      style: Styles.styleText20,
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    const Text(
+                      'youssef abdallah youssef abdallah youssef abdallah youssef abdallah youssef abdallah youssef abdallah youssef abdallah youssef abdallah',
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      children: [
+                        const Text('9.9 / 10'),
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 15,
+                          ),
+                          child: Text(
+                            '2024-10-12',
+                            style: Styles.styleText16
+                                .copyWith(color: Colors.white.withOpacity(0.5)),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
